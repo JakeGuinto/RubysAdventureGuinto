4d8d5c5ab769fc759f9a190ea4265815d23d08cf
JakeGuinto <116121157+JakeGuinto@users.noreply.github.com>
2022-11-03 15:25:45 -0700

    scripts and tiles
    added first file and added up  down and left and right movement

--- RubysAdventureGuintoP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureGuintoP7/Assets/Scripts/RubyController.cs
@@ -7,18 +7,25 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("horizontal");
+        //create variables to use thr built in axis
+        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
+       
+        //Create a movement vector
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+
+        //make horizontal and vertical movement work
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
         transform.position = position;
+
     }
 }
